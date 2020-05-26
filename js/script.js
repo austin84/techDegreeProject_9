57c1ce121b9bd0593b2d071f36d2d95f31dc3da4
austin84 <austinveatch84@gmail.com>
2020-05-25 21:03:57 -0700

    added lax plugin and styled arrow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Initialize lax plugin
+window.onload = function () {
+  lax.setup(); // init
+
+  const updateLax = () => {
+    lax.update(window.scrollY);
+    window.requestAnimationFrame(updateLax);
+  };
+
+  window.requestAnimationFrame(updateLax);
+};
+
 // Hamburger
 
 const menuBtn = document.querySelector('.menu-btn');
