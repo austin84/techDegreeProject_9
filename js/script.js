78e70a8a14c667e61a8ed9578fe418566afdc1b5
austin84 <austinveatch84@gmail.com>
2020-05-27 10:53:49 -0700

    sass organization

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // Initialize lax plugin
 window.onload = function () {
-  lax.setup(); // init
+  lax.setup({
+    breakpoints: { small: 0, large: 924 },
+  }); // init
 
   const updateLax = () => {
     lax.update(window.scrollY);
@@ -25,3 +27,41 @@ menuBtn.addEventListener('click', (e) => {
 });
 
 // !  Working on LAX sliding the portfolio!!!!
+
+/**
+ *
+ * @param {element} target - landing for smoothscroll event
+ * @param {*} duration - length of time for smoothscroll event
+ * Causes smooth scroll from top of page to the about section
+ */
+
+function smoothScroll(target, duration) {
+  const end = document.querySelector(target);
+  const endPosition = end.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
+  const distance = endPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime == null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+const arrow = document.querySelector('#arrow');
+
+arrow.addEventListener('click', (e) => {
+  smoothScroll('.about', 1400);
+});
