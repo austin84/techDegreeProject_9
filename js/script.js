1ee2d4c4e882f21cc07679d93e372ef178a3724b
austin84 <austinveatch84@gmail.com>
2020-05-27 15:17:06 -0700

    modal functions, no styling

--- js/script.js
+++ js/script.js
@@ -93,3 +93,26 @@ window.onclick = function (event) {
     }
   }
 };
+
+// ! ////
+// Modal Display
+
+MicroModal.init({
+  onShow: (modal) => console.info(`${modal.id} is shown`), // [1]
+  onClose: (modal) => console.info(`${modal.id} is hidden`), // [2]
+  openTrigger: 'data-custom-open', // [3]
+  closeTrigger: 'data-custom-close', // [4]
+  openClass: 'is-open', // [5]
+  disableScroll: true, // [6]
+  disableFocus: false, // [7]
+  awaitOpenAnimation: true, // [8]
+  awaitCloseAnimation: true, // [9]
+  debugMode: false, // [10]
+});
+
+for (let i = 0; i < 6; i++) {
+  const port = document.querySelector(`.port-${i + 1}`);
+  port.addEventListener('click', (e) => {
+    MicroModal.show(`modal-${i + 1}`);
+  });
+}
